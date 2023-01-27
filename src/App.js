93d3d0bb09cb296908c6891c70fa9bcc892fb47f
kth <kth@kth-main>
2023-01-27 23:10:57 +0900

    i1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React  from 'react';
-import { TopNav, SearchAndToggler, PostHeader, PostContent, SideNav, PageDimmer, Masonry, NotFound, Footer } from './element/';
-//import { Route, Switch } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
+import "./App.css";
+import Router from "./Router";
 
-class App extends element {
-  render() {
-    return (
-       <div>
-         {/* <TopNav />
-         <SearchAndToggler />
-         <PageDimmer />
+function App() {
+  return (
+    <div className='App'>
+      <Router />
+    </div>
+  );
+}
 
-        <SideNav> */}
-          {/* <Route path='/' element={PostHeader} /> */}
-          <Routes>
-            {/* <Route path='/search/:query' element={< Masonry />} />
-            <Route path='/category/:query' element={< Masonry />} />
-            <Route path='/PageNotFound' exact element={<NotFound />} />
-            <Route path='/:postId' element={< PostContent />} /> */}
-            <Route path='/'  element={< PostContent /> } />
-          </Routes>
-          <Footer />
-        {/* </SideNav> */}
-      </div>
-    )
-  }
-};
-
-export default App
+export default App;
