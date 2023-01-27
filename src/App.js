afa2cad55b4ee3f3cb3e9ec2b9e0b9ddcbfd335c
kth <kth@kth-main>
2023-01-28 00:53:13 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import "./App.css";
-import Router from "./Router";
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="black-nav">
+//         <h4>xxxx</h4>
+//       </div>
+//     </div>
+//   );
+// }
+// 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn  React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+ import "./App.css";
+ import Router from "./Components/Router/Router";
+ 
+ function App() {
   return (
-    <div className='App'>
-      <Router />
+     <div className='App'>
+      { <Router /> }
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import AboutPage from "./Pages/About/AboutPage";
+import ContactPage from "./Pages/Contact/ContactPage";
+import StartPage from "./Pages/Start/StartPage";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/'>
+          Start
+        </NavLink>
+        <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/about'>
+          About
+        </NavLink>
+        <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/contact'>
+          Contact
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route exact path='/' element={<StartPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
